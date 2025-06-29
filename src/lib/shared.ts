f794a949ac47de8ce594d94380423b797be12496
bunyawat.n <bunyawatnaunnak@gmail.com>
2025-06-29 15:02:19 +0700

    fix: api url

--- src/lib/shared.ts
+++ src/lib/shared.ts
@@ -1,4 +1,4 @@
 // API base URL configuration
 export const API_BASE_URL = import.meta.env.DEV
 	? 'http://localhost:4000/api'
-	: 'https://cusw-workspace.chula.ac.th/api';
+	: 'https://cusw-workspace.sa.chula.ac.th/api';
